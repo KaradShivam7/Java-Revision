08d1f1317f6ed09d7e06f046b43cbd6a11cdc161
KaradShivam7 <shivamkarad20@gmail.com>
2026-05-10 23:27:56 +0530

    Java

---
+++ Basic/src/Condition_statement/if_.java
@@ -0,0 +1,13 @@
+package Condition_statement;
+
+public class if_ {
+	
+	public static void main(String[] args) {
+		int age=19;
+		
+		if(age>18) {
+			System.out.println("adult!!!");
+		}
+	}
+
+}
---
+++ Basic/src/Condition_statement/if_else.java
@@ -0,0 +1,19 @@
+package Condition_statement;
+
+public class if_else {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int age=2;
+		
+		if(age>18) {
+			System.out.println("Adult!!!");
+		}
+		else {
+			System.out.println("child!!!");
+		}
+
+	}
+
+}
