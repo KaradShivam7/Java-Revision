95497dcb15f729453ce3f874b2dbf2c4734f6d37
KaradShivam7 <shivamkarad20@gmail.com>
2026-05-12 23:29:23 +0530

    Java

---
+++ Basic/src/Basic_java/Variable1.java
@@ -0,0 +1,12 @@
+package Basic_java;
+
+public class Variable1 {
+
+	static int i;
+	static char ch;
+	public static void main(String[] args) {
+		
+		System.out.println(i);
+		System.out.println(ch);
+	}
+}
